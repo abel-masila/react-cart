a8cab0276eaf1ee9bf8c8c40c9e1b111fe45e04c
Abel Masila <abelmasila@gmail.com>
2021-05-21 01:35:15 +0300

    Item component done

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import { useState } from "react";
 import { useQuery } from "react-query";
 
 //components
+
 import Drawer from "@material-ui/core/Drawer";
 import LinearProgress from "@material-ui/core/LinearProgress";
 import Grid from "@material-ui/core/Grid";
 import AddShoppingCart from "@material-ui/icons/AddShoppingCart";
 import Badge from "@material-ui/core/Badge";
+
+import Item from "./Item/Item";
 //styles
 import { Wrapper } from "./App.styles";
 
@@ -31,15 +34,21 @@ function App() {
   );
 
   const getTotalItems = () => null;
-  const handleAddToCart = () => null;
+  const handleAddToCart = (clickedItem: CartItemType) => null;
   const handleRemoveFromCart = () => null;
 
   if (isLoading) return <LinearProgress />;
   if (error) return <div>Something went wrong...</div>;
   return (
-    <div className="App">
-      <h3>Hey</h3>
-    </div>
+    <Wrapper>
+      <Grid container spacing={3}>
+        {data?.map((item) => (
+          <Grid item key={item.id} xs={12} sm={4}>
+            <Item item={item} handleAddToCart={handleAddToCart} />
+          </Grid>
+        ))}
+      </Grid>
+    </Wrapper>
   );
 }
 
